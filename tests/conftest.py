192c8c6fbccfbc03d338004c81a736ddb9fb7a68
Nailia Aleshicheva <gilmanova-nailya@mail.ru>
2025-04-19 22:47:34 +0300

    Тесты demoqa со средним PageObject

---
+++ tests/conftest.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+import pytest
+from selene import browser
+from selenium import webdriver
+
+
+
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+
+@pytest.fixture(scope="function", autouse=True)
+def browser_config():
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = "eager"
+    # driver_options.add_argument("--headless")
+    browser.config.driver_options = driver_options
+    browser.config.type_by_js = True
+
+    browser.config.window_height = 2500
+    browser.config.window_width = 1400
+
+    browser.config.base_url = "https://demoqa.com/automation-practice-form"
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture(scope="function")
+def today_date():
+    c = datetime.now()
+    current_time = c.strftime("%d %B, %Y")
---
+++ tests/test_hw.py
@@ -0,0 +1,58 @@
+from model.pages.practice_form import RegistrationPage
+
+
+def test_form_submitted():
+    practice_form = RegistrationPage()
+    (practice_form.open()
+    .fill_first_name("Maria")
+    .fill_last_name("Lopez")
+    .fill_email("MLopez@gmail.com")
+    .select_gender("Female")
+    .fill_mobile_number("0123456789")
+    .fill_date_of_birth(9, 1996, 10)
+    .fill_subject('Biology')
+    .set_avatar("unnamed.jpg")
+    .select_hobbies("Reading")
+    .select_hobbies("Music")
+    .fill_current_address("Main street, 55 bld, 10 apt.")
+    .select_state("Rajasthan")
+    .select_city("Jaipur")
+    .submit_form()
+    .registered_user_should_have(
+        "Maria", "Lopez", "MLopez@gmail.com", "Female", "0123456789", "10 October,1996", "Biology", "Reading, Music",
+        "unnamed.jpg", "Main street, 55 bld, 10 apt.", "Rajasthan", "Jaipur"
+    ))
+
+
+def test_form_required_fields_only(today_date):
+    practice_form = RegistrationPage()
+    (practice_form.open()
+    .fill_first_name("Maria")
+    .fill_last_name("Lopez")
+    .select_gender("Female")
+    .fill_mobile_number("0123456789")
+    .submit_form()
+    .registered_user_should_have(
+        "Maria", "Lopez", "", "Female", "0123456789", f"c", "", "",
+        "", "", "", ""
+    ))
+
+
+def test_form_error_required_fields_not_filled():
+    practice_form = RegistrationPage()
+    (practice_form.open()
+     .fill_first_name("Maria")
+     .select_gender("Female")
+     .submit_form()
+     .check_if_required_fields_not_filled())
+
+
+def test_form_required_fields_but_wrong_number():
+    practice_form = RegistrationPage()
+    (practice_form.open()
+     .fill_first_name("Maria")
+     .fill_last_name("Lopez")
+     .select_gender("Female")
+     .fill_mobile_number("123456789")
+     .submit_form()
+     .check_if_required_fields_not_filled())
